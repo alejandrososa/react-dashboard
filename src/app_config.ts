0242dc5acebcdb5e34356a8c6d33e0f15d46244f
Alejandro Sosa <alesjohnson@hotmail.com>
2023-03-30 16:41:49 +0200

    Show the list of widgets on the dashboard

--- src/app_config.ts
+++ src/app_config.ts
@@ -11,15 +11,15 @@ export const config: AppConfig = {
 	widgets: [
 		{
 			id: "8e35c69a-c42d-45b2-90b2-a538664bf436",
-			repository_url: "https://github.com/alejandrososa/mower"
+			repository_url: "https://github.com/alejandrososa/mower",
 		},
 		{
 			id: "6c57a193-d230-434a-98f9-643967f28f94",
-			repository_url: "https://github.com/alejandrososa/discounts"
+			repository_url: "https://github.com/alejandrososa/discounts",
 		},
 		{
 			id: "b988be1d-e26d-4d3a-b538-1cbb4eac0e80",
-			repository_url: "https://github.com/alejandrososa/dockbox"
+			repository_url: "https://github.com/alejandrososa/dockbox",
 		},
-	]
+	],
 };
--- src/sections/dashboard/Dashboard.tsx
+++ src/sections/dashboard/Dashboard.tsx
@@ -1,3 +1,19 @@
+import { config } from "../../app_config";
+
 export function Dashboard() {
-	return <div>Dashboard</div>;
+	const title = "Dashboard";
+	const widgets = config.widgets;
+
+	return (
+		<>
+			<div>
+				<h1>{title}</h1>
+				<ul>
+					{widgets.map((widget) => (
+						<li key={widget.id}>{widget.repository_url}</li>
+					))}
+				</ul>
+			</div>
+		</>
+	);
 }
